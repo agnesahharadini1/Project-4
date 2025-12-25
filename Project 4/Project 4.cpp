1f9812ecbf1e5c2a32096c7d0374b00c165dd68b
Agnesa Haradini <agnesa.haradini1@m.uni-pr.edu>
2025-12-25 23:11:52 +0100

    Kam krijuar strukturen Student

--- Project 4/Project 4.cpp
+++ Project 4/Project 4.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+struct Student {
+    int id;
+    string emri;
+    string drejtimi;
+    double notaMesatare;
+};
