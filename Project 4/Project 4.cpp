1b842f46257f8707583903118ff284adcaa33de3
agnesahharadini1 <agnesa.haradini1@student.uni-pr.edu>
2025-12-25 23:18:07 +0100

    Update Project 4.cpp
     Funksioni per shtimin e studentit

--- Project 4/Project 4.cpp
+++ Project 4/Project 4.cpp
@@ -10,3 +10,23 @@ struct Student {
     string drejtimi;
     double notaMesatare;
 };
+
+vector<Student> studentet;
+void shtoStudent() {
+    Student s;
+    cout << "\nID: ";
+    cin >> s.id;
+    cin.ignore();
+
+    cout << "Emri: ";
+    getline(cin, s.emri);
+
+    cout << "Drejtimi: ";
+    getline(cin, s.drejtimi);
+
+    cout << "Nota Mesatare: ";
+    cin >> s.notaMesatare;
+
+    studentet.push_back(s);
+    cout << "\nStudenti u shtua me sukses!\n";
+}
